3991580dd43cef9673d9c3c50cd6725b33855b79
Wallace Wong <wallace.w1997@gmail.com>
2021-12-08 16:13:40 -0500

    Day2Part1 Complated

---
+++ AdventOfCode/Day2/Day2Part1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day2
+{
+    internal static class Day2Part1
+    {
+        private static string inputfile = @".\Day2\input.txt";
+
+        private static Dictionary<char, int> MoveShip()
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>()
+            {
+                {'x', 0 },
+                {'y', 0 }
+            };
+            foreach (string line in File.ReadAllLines(inputfile))
+            {
+                string[] templine = line.Split(' ');
+                string direction = templine[0];
+                int steps = int.Parse(templine[1]);
+
+                if (direction == Direction.Forward)
+                {
+                    result['x'] += steps;
+                }
+                else if (direction == Direction.Up)
+                {
+                    result['y'] -= steps;
+                }
+                else if (direction == Direction.Down)
+                {
+                    result['y'] += steps;
+                }
+            }
+
+            //Attemped at a parallel for loop seems to skip a lot of the steps
+            //Parallel.ForEach(File.ReadLines(inputfile), line =>
+            //{
+            //    string[] templine = line.Split(' ');
+            //    string direction = templine[0];
+            //    int steps = int.Parse(templine[1]);
+
+            //    if (templine[0] == Direction.Forward)
+            //    {
+            //        result['x'] += int.Parse(templine[1]);
+            //    }
+            //    else if (templine[0] == Direction.Up)
+            //    {
+            //        result['y'] -= int.Parse(templine[1]);
+            //    }
+            //    else if (templine[0] == Direction.Down)
+            //    {
+            //        result['y'] += int.Parse(templine[1]);
+            //    }
+
+            //});
+
+            return result;
+        }
+
+        public static void GetAnswer()
+        {
+            var answer = MoveShip();
+            Console.WriteLine("Day2 Part 1: {0}", answer['x'] * answer['y']);
+        }
+    }
+
+    internal static class Direction
+    {
+        public static string Forward => "forward";
+        public static string Up => "up";
+        public static string Down => "down";
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,9 @@
 ﻿using AdventOfCode.Day1;
+using AdventOfCode.Day2;
 
 Day1.GetDay1Answer();
 Day1p2.GetDay1Answer();
 
 Console.WriteLine("");
+
+Day2Part1.GetAnswer();
