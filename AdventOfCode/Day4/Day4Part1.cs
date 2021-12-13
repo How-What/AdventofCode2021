75df6d27ee6cfe1a912e77b5784eeb29741008b2
Wallace Wong <wallace.w1997@gmail.com>
2021-12-12 19:45:19 -0500

    Starts on Day 4 Part 1 Brain Power Too Low Will finish someother time

---
+++ AdventOfCode/Day4/Day4Part1.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day4
+{
+    internal static class Day4Part1
+    {
+        private static string inputfile = @".\Day4\input.txt";
+        private static string[] bingoCalls = File.ReadLines(inputfile).First().Split(',');
+
+        internal static void GetAnswer()
+        {
+            var boards = GetBingoBoards();
+            var board = GetBingoBoard(boards);
+            var bingoBoard = GetIndividualBoard(board);
+            
+        }
+
+        private static List<string> GetBingoBoards()
+        {
+            bool firstpass = true;
+            string pattern = @"([0-9])+";
+
+            List<string> list = new List<string>();
+            foreach (string line in File.ReadLines(inputfile))
+            {
+                Match match = Regex.Match(line, pattern);
+
+                if (firstpass)
+                {
+                    firstpass = false;
+                    continue;
+                }
+                if (match.Success)
+                {
+                    list.Add(line);
+                }
+            }
+
+            return list;
+        }
+
+        private static List<Dictionary<int, bool>> GetBingoBoard(List<string> boards)
+        {
+            List<Dictionary<int, bool>> retres = new List<Dictionary<int, bool>>();
+            Dictionary<int, bool> result = new Dictionary<int,bool>();
+            foreach (string line in boards)
+            {
+                foreach (var i in Regex.Split(line, @"\s+"))
+                {
+                    try
+                    {
+                        result.Add(Int32.Parse(i), false);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    
+                }
+                retres.Add(result);
+                result = new Dictionary<int,bool>();
+            }
+
+            return retres;
+        }
+        private static List<List<Dictionary<int, bool>>> GetIndividualBoard(List<Dictionary<int, bool>> board)
+        {
+            var result = new List<List<Dictionary<int,bool>>>();
+
+            for(int i = 0; i < board.Count; i+=5)
+            {
+                result.Add(board.GetRange(i, 5));
+            }
+
+            return result;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,6 +2,7 @@
 using AdventOfCode.Day1;
 using AdventOfCode.Day2;
 using AdventOfCode.Day3;
+using AdventOfCode.Day4;
 
 Day1.GetDay1Answer();
 Day1p2.GetDay1Answer();
@@ -15,3 +16,5 @@ Console.WriteLine("");
 
 Day3Part1.GetAnswer();
 Day3part2.GetAnswer(); 
+
+Day4Part1.GetAnswer();
